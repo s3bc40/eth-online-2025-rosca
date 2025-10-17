5ad105893163649683dbe39cde7cd88feff1f801
s3bc40 <s3bc40@proton.me>
2025-10-17 12:51:23 +0200

    feat: hook safeKit with safe contracts on local anvil

--- apps/web/app/pages/my-roscas/page.tsx
+++ apps/web/app/pages/my-roscas/page.tsx
@@ -1,12 +1,45 @@
+"use client";
+
+// import { useEffect, useState } from "react";
 import Navbar from "../../components/Navbar";
+// import useSafeProtocolKit from "../../hooks/useSafeProtocolKit";
 
 export default function RoscaList() {
+  // // ------------ TO REMOVE ------------
+  // const { safeKit, initSafeProtocolKit } = useSafeProtocolKit();
+  // const [safeAddress, setSafeAddress] = useState<string | undefined>();
+  // const [isDeployed, setIsDeployed] = useState<boolean | undefined>();
+
+  // useEffect(() => {
+  //   initSafeProtocolKit(
+  //     [
+  //       "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
+  //       "0x70997970C51812dc3A010C7d01b50e0d17dc79C8",
+  //     ],
+  //     2
+  //   );
+  // }, [initSafeProtocolKit]);
+
+  // useEffect(() => {
+  //   if (!safeKit) return;
+  //   (async () => {
+  //     setSafeAddress(await safeKit.getAddress());
+  //     setIsDeployed(await safeKit.isSafeDeployed());
+  //   })();
+  // }, [safeKit]);
+  // // ------------ TO REMOVE ------------
+
   return (
     <div>
       <Navbar />
       <div className="flex flex-col items-center justify-center min-h-screen bg-gray-50">
         <h1 className="text-3xl font-bold text-gray-800">Rosca List</h1>
         <p className="mt-4 text-gray-600">Here are your Rosca groups.</p>
+        {/* // ------------ TO REMOVE ------------ */}
+        {/* <p className="mt-10 text-orange-800">TEST SAFE KIT</p> */}
+        {/* <p className="mt-4 text-orange-600">{safeAddress}</p> */}
+        {/* <p className="mt-4 text-orange-600">{String(isDeployed)}</p> */}
+        {/* // ------------ TO REMOVE ------------ */}
       </div>
     </div>
   );
