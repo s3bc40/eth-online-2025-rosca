cbacdec4474194fda15c26a2ec186fdb9460e323
laitanop <laitanop@yahoo.com>
2025-10-22 21:39:33 -0600

    fix private key anvil

--- apps/web/app/scripts/fundEthAnvil.js
+++ apps/web/app/scripts/fundEthAnvil.js
@@ -29,8 +29,7 @@ const anvilChain = {
 };
 
 async function sendETH() {
-  const privateKey =
-    "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
+  const privateKey = "Anvil private key";
   const toAddress = "YOUR_ADDRESS";
   const amount = "1.5";
 
