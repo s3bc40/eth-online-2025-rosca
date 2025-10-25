c0280305298141ad5cb8d6e3191b4576a4cc876c
s3bc40 <32032033+s3bc40@users.noreply.github.com>
2025-10-25 16:51:13 +0200

    fix: fix chain id ARB sepolia

--- apps/web/app/utils/helpers.ts
+++ apps/web/app/utils/helpers.ts
@@ -1,6 +1,6 @@
 // Helpers for the ROSCA DApp
 import FactoryAnvil from "../../../foundry/deployments/anvil/Factory.json";
-import FacotryArbSepolia from "../../../foundry/deployments/arbitrum-sepolia/Factory.json";
+import FactoryArbSepolia from "../../../foundry/deployments/arbitrum-sepolia/Factory.json";
 
 /**
  * Get the factory contract address based on the chain ID
@@ -13,7 +13,7 @@ export function getFactoryContractAddress(
 ): `0x${string}` | undefined {
   const addresses: { [key: number]: `0x${string}` } = {
     31337: FactoryAnvil.address as `0x${string}`, // Anvil
-    421613: FacotryArbSepolia.address as `0x${string}`, // Arbitrum Sepolia
+    421614: FactoryArbSepolia.address as `0x${string}`, // Arbitrum Sepolia
   };
   return addresses[chainId];
 }
